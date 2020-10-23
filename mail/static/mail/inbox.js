d7241c1ee6c16c19d0c1f4701fe619c0ef44683a
Carlo Sanchez <scarlos@unicauca.edu.co>
2020-10-22 21:23:47 -0500

    ver correo electronico funcional

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,26 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
-  
-  document.querySelector('form').onsubmit = function () {
-    
-    fetch('/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-        recipients: document.querySelector('#compose-recipients').value,
-        subject: document.querySelector('#compose-subject').value,
-        body: document.querySelector('#compose-body').value 
-      })
-    })
-      .then(response => response.json())
-      .then(result => {
-        // Print result
-        console.log(result);
-      });
-    }
+
+
+
   // By default, load the inbox
   load_mailbox('inbox');
+
+
+  
+
+
 });
+
+
+
 
 
 function compose_email() {
@@ -40,12 +34,12 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 
 
-  
 
-      
 
-  
-  
+
+
+
+
 }
 
 function load_mailbox(mailbox) {
@@ -61,24 +55,59 @@ function load_mailbox(mailbox) {
     .then(emails => {
       // Print emails
       console.log(emails);
+
       var caja = document.getElementById('emails-view');
       emails.forEach(email => {
-        // const div = document.createElement('div',"email-cont");
-        // div.innerHTML = email.id;
-        
-        caja.innerHTML += '<div class="email-cont">' + 
-        '<p class="email-user"> '+  email.sender + '</p>'+
-        '<p class="email-subject"> '+  email.subject + '</p>'+
-        '<p class="email-body"> '+  email.body + '</p>'+   
-        '</div>';
+
+        if (email.read == true) {
+          var back_color = "rgb(209, 209, 209);";
+        } else {
+          var back_color = "white";
+        }
+        caja.innerHTML += '<div class="email-cont" style="background-color:' + back_color + ';">' +
+          
+          '<p  onclick="show_email('+ email.id +');" class="email-user" > ' + email.sender + '</p>' +
+          '<p class="email-subject"> ' + email.subject + '</p>' +
+          '<p class="email-body"> '+ email.body + '</p>' +
+          '</div>';
         //document.querySelector('#emails-view').append(div);
-        
+
       });
       // ... do something else with emails ...
     });
 
   // Show the mailbox name
+
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+}
+
+
+function show_email(id) {
+
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#emails-view').innerHTML = "";
+
+  //document.querySelector('#test').innerHTML=`<h2> Mensaje de prueba el id es ${id} </2>`;
   
- 
+  fetch('/emails/'.concat(id))
+    .then(response => response.json())
+    .then(email => {
+      // Print emails
+      console.log(email);
+
+      var caja = document.getElementById('emails-view');
+      var back_color = "white";
+      // ... do something else with email ...
+
+    caja.innerHTML = '<div class="email-cont" style="background-color:' + back_color + ';">' +
+          
+          '<p  onclick="show_email('+ email.id +');" class="email-user" > ' + email.sender + '</p>' +
+          '<p class="email-subject"> ' + email.subject + '</p>' +
+          '<p class="email-body"> '+ email.body + '</p>' +
+          '</div>';
+    });
+  
 }
