a236e821dcde76898db6639fb507800c088839da
Carlo Sanchez <scarlos@unicauca.edu.co>
2020-10-25 02:13:48 -0500

    primera version terminada

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,26 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#send-button').addEventListener('click', () => {
+
+    console.log('Enviando email');
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.getElementById('compose-recipients').value,
+        subject: document.getElementById('compose-subject').value,
+        body: document.getElementById('compose-body').value
+      })
+    })
+      .then(response => response.json())
+      .then(result => {
+        // Print result
+        console.log(result);
+        load_mailbox('inbox');
+      });
+      
+  });
+
   //close popup
   // document.querySelector('#btn-close').addEventListener('click', ()=>{
   //   document.querySelector('.overlay').style.display = 'none';
@@ -33,7 +53,7 @@ function compose_email() {
   document.querySelector('.overlay').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-  
+
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -41,11 +61,7 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 
 
-
-
-
-
-
+  
 
 }
 
@@ -56,6 +72,9 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#emails-view').innerHTML = "";
+
+  //enviar
+
 
   //peticion get
   fetch('/emails/'.concat(mailbox))
@@ -72,7 +91,7 @@ function load_mailbox(mailbox) {
         } else {
           var back_color = "white";
         }
-        caja.innerHTML += '<div class="email-cont" style="background-color:' + back_color + ';">' +
+        caja.innerHTML += '<div id="email-'+ email.id+'" class="email-cont" style="background-color:' + back_color + ';">' +
 
           '<p  onclick="show_email(' + email.id + ');" class="email-user" > <strong> ' + email.sender +
           ' </strong>' + email.subject + '</p>' +
@@ -81,14 +100,41 @@ function load_mailbox(mailbox) {
           '</div>';
         //document.querySelector('#emails-view').append(div);
 
+        if (mailbox == 'archive') {
+
+          const btn_undo = document.createElement('button');
+          btn_undo.className = 'btn btn-sm btn-outline-primary';
+          btn_undo.innerHTML = 'Unarchive';
+          btn_undo.setAttribute("onclick", "unarchive_email("+ email.id+");");
+
+          // btn_undo.addEventListener('click', function () {
+            
+          // });
+
+          document.querySelector('#email-'+email.id).append(btn_undo);
+
+
+        }
       });
       // ... do something else with emails ...
+
     });
+
 
   // Show the mailbox name
 
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+}
+
+function unarchive_email(id){
+  fetch('/emails/'.concat(id), {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: false
+    })
+  });
+  load_mailbox('inbox');
 }
 
 
@@ -111,7 +157,7 @@ function show_email(id) {
       var back_color = "white";
       // ... do something else with email ...
 
-      caja.innerHTML = '<div >' +
+      caja.innerHTML = '<div ">' +
 
         '<p id="TX"> <strong> From: </strong>' + email.sender + '</p>' +
         '<p id="RX"> <strong> To: </strong>' + email.recipients + '</p>' +
@@ -119,7 +165,7 @@ function show_email(id) {
         '<p id="time"> <strong> Timestamp: </strong>' + email.timestamp + '</p>' +
         '<div class="flex-cont">' +
         '<button onclick="reply_email(' + email.id + ');"class="btn btn-sm btn-outline-primary">Reply</button>' +
-        '<button onclick="archived_email('+ email.id +');"class="btn btn-sm btn-outline-primary">Archived</button>' +
+        '<button onclick="archived_email(' + email.id + ');"class="btn btn-sm btn-outline-primary">Archived</button>' +
         '</div>' +
         '</div>' +
         '<hr color="blue" size=3>' +
@@ -157,15 +203,19 @@ function reply_email(id) {
 }
 
 
-function archived_email(id){
+function archived_email(id) {
   fetch('/emails/'.concat(id), {
     method: 'PUT',
     body: JSON.stringify({
-        archived: true
+      archived: true
     })
   });
   document.querySelector('.overlay').style.display = 'flex';
-  document.querySelector('#btn-close').addEventListener('click', ()=>{
+  document.querySelector('#btn-close').addEventListener('click', () => {
+    document.querySelector('.overlay').style.display = 'none';
+  });
+  document.querySelector('#btn-undo').addEventListener('click', () => {
+    unarchive_email(id);
     document.querySelector('.overlay').style.display = 'none';
   });
 }
